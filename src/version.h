7745d837f9ad935a0b1e7604254c429f9c2cb49e
samvicky <billavignesh26@gmail.com>
2018-11-10 13:00:28 +0530

    The Code

---
+++ src/version.h
@@ -0,0 +1,6 @@
+#pragma once
+
+extern const char* const KREDITS_VERSION_TAG;
+extern const char* const KREDITS_VERSION;
+extern const char* const KREDITS_RELEASE_NAME;
+extern const char* const KREDITS_VERSION_FULL;
