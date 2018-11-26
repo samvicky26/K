e40656fcd975fc1cb53d1482aee4572b0f8808d6
samvicky <billavignesh26@gmail.com>
2018-11-27 01:18:44 +0530

    Fixed the emission

--- src/cryptonote_basic/cryptonote_basic_impl.cpp
+++ src/cryptonote_basic/cryptonote_basic_impl.cpp
@@ -107,8 +107,8 @@ namespace cryptonote {
       base_reward = 0;
     }
 
-   /* if (version >= 8)
-      base_reward = 28000000000.0 + 100000000000.0 / kredits_exp2(height / (720.0 * 90.0)); // halve every 90 days. */
+    if (version >= 8)
+      base_reward = 900000000000.0 + 1000000000000.0 / kredits_exp2(height / (720.0 * 90.0)); // halve every 90 days.
 
     uint64_t full_reward_zone = get_min_block_size(version);
 
